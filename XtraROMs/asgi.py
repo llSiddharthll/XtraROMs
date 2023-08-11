276484ad455ab872f1b540acb3ce83826fc5d11b
Siddharth Tiwari <95074087+llSiddharthll@users.noreply.github.com>
2023-08-11 01:30:44 +0000

    Updated 👍👍

--- XtraROMs/asgi.py
+++ XtraROMs/asgi.py
@@ -1,18 +1,18 @@
-"""
-ASGI config for SidXFleur project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
+from hypercorn.config import Config
+from hypercorn.asyncio import serve
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'XtraROMs.settings')
 
 application = get_asgi_application()
 
-app = application
+# This is the entry point that Hypercorn will use
+def run_hypercorn():
+    config = Config()
+    config.bind = [f'{os.environ.get("HOST", "0.0.0.0")}:{os.environ.get("PORT", "8000")}']
+    serve(application, config)
+
+# Start the Hypercorn server
+if __name__ == "__main__":
+    run_hypercorn()
