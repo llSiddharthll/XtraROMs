c14776d0eee4ecc833bce771a7c6bb23effa72e0
llSiddharthll <siddharthgreat443@gmail.com>
2023-08-11 21:27:40 +0530

    Updated_again 👍👍

--- XtraROMs/settings.py
+++ XtraROMs/settings.py
@@ -121,7 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'), ) 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
@@ -130,8 +131,8 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URl = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
