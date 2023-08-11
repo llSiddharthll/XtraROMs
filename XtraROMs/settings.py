bdd4faef2f48a5ebe82ceda49a45491bb77dccac
llSiddharthll <siddharthgreat443@gmail.com>
2023-08-12 04:15:27 +0530

    Latest_Build 👍👍

--- XtraROMs/settings.py
+++ XtraROMs/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*",".vercel.app",".now.sh"]
 
 
 # Application definition
--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,5 @@
 echo "BUILD START"
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
+sudo apt-get install libsqlite3-dev
 echo "BUILD DONE"
