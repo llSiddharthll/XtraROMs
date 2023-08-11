8f86a96784f4010ce31762890fed65fc0eb4dff9
llSiddharthll <siddharthgreat443@gmail.com>
2023-08-11 21:40:54 +0530

    Updated 👍👍

--- XtraROMs/settings.py
+++ XtraROMs/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
@@ -120,9 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'), ) 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATIC_URL = "/static/"
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static/'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static/')
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
