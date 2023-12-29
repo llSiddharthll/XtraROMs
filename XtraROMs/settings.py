6729557545e4cb978d447c9cca6e27118844a32b
llSiddharthll <siddharthgreat443@gmail.com>
2023-12-29 10:03:59 +0530

    done

--- XtraROMs/settings.py
+++ XtraROMs/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
