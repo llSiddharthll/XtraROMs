44854a57e1d7887f7ac3ccd9796ae87400036f53
llSiddharthll <siddharthgreat443@gmail.com>
2024-02-24 20:23:43 +0530

    ok

--- XtraROMs/settings.py
+++ XtraROMs/settings.py
@@ -30,7 +30,6 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,8 +44,7 @@ INSTALLED_APPS = [
     'crispy_bootstrap5',
     'allauth',
     'allauth.account',
-    'allauth.socialaccount',
-    
+    'allauth.socialaccount',  
 ]
 
 MIDDLEWARE = [
